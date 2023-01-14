d51345412a11e139181bdc533d8e6a3c48c3bd84
Thomas Walker <thomas.mattia.walker@hotmail.co.uk>
2023-01-14 16:32:32 +0000

    Link Iterator

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -20,3 +20,22 @@ class Link:
         """Insert a new link after this one."""
         link.next = self.next
         self.next = link
+
+    def __iter__(self):
+        return LinkIterator(self)
+
+
+class LinkIterator:
+    def __init__(self, link):
+        self.link = link
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if not self.link:
+            raise StopIteration
+
+        value = self.link.value
+        self.link = self.link.next
+        return value
