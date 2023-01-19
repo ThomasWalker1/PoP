69a66cdf3efb33b5c0b3fe857f3ec18f2060f3c3
Thomas Walker <thomas.mattia.walker@hotmail.co.uk>
2023-01-19 10:53:29 +0000

    Graphs Update

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -1,3 +1,6 @@
+from functools import singledispatch
+
+
 class TreeNode:
     """"
     A basic tree implementation.
@@ -58,3 +61,50 @@ def previsitor(tree, fn, fn_parent=None):
 
     for child in tree.children:
         previsitor(child, fn, fn_out)
+
+
+@singledispatch
+def evaluate(expr, *o, **kwargs):
+    """
+    Evaluate an expression node.
+
+    Parameters
+    ----------
+    expr: Expression
+        The expression node to be evaluated
+    *o: numbers.Number
+        The results of evaluating the operands of expr
+    **kwargs:
+        Any keyword arguments required to evaluate specific types of
+        expression.
+    symbol_map: dict
+        A dictionary mapping Symbol names to numerical values, for
+        example:
+        {'x': 1}
+    """
+    raise NotImplementedError(
+        f"Cannot evaluate a{type(expr).__name__}")
+
+# Complete expressions package
+
+
+def postvisitor_expanded(expr, fn, **kwargs):
+    '''
+    Visit an Expression in post-order applying a function every node
+
+    Parameters
+    ----------
+    expr: Expression
+        The expression to be visited.
+    fn: function(node, *0, **kwargs)
+        A function to be applied at each node. The function should take
+        the node to be visited as its first argument, and the result of
+        visiting its operands as any further positional arguments. Any
+        additional information that the visitor requires can be passed
+        as keyword arguments.
+    **kwargs:
+        Any additional keyword arguments to be passed to fn.
+    '''
+    return fn(expr,
+              *(postvisitor_expanded(c, fn, **kwargs) for c in expr.operands),
+              **kwargs)
